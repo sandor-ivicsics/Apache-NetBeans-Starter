915035b1b91fe6027e9273d7a1f1b0ca0d3f6c59
Sándor Ivicsics <sandor.ivicsics@gmail.com>
2019-07-29 22:09:05 +0200

    userdefaults

--- Apache NetBeans Starter/AppDelegate.swift
+++ Apache NetBeans Starter/AppDelegate.swift
@@ -10,47 +10,32 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    let JDKS_DIRECTORY = URL(fileURLWithPath: "/Library/Java/JavaVirtualMachines")
-
+    
     @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        let openPanel = NSOpenPanel()
-        openPanel.allowsMultipleSelection = false
-        openPanel.canChooseDirectories = true
-        openPanel.canChooseFiles = false
-        openPanel.title = "Please, select Apache NetBeans home directory!"
-        if (openPanel.runModal() != .OK) {
-            return
+        let userDefaults = UserDefaults.standard
+        var netbeansJDKHome = userDefaults.string(forKey: "netbeans_jdk_home")
+        if netbeansJDKHome == nil {
+            netbeansJDKHome = getInstalledJDKHome(installedJDK: selectInstalledJDK())
+            if netbeansJDKHome == nil {
+                return
+            }
+            userDefaults.set(netbeansJDKHome, forKey: "netbeans_jdk_home")
         }
-        guard let netbeansHome = openPanel.urls.first else {
-            return
+        print("netbeansJDKHome: \(netbeansJDKHome ?? "")")
+        var netbeansShellScript = getNetBeansShellScript(netbeansHome: userDefaults.url(forKey: "netbeans_home"))
+        if netbeansShellScript == nil {
+            let netbeansHome = selectNetBeansHome()
+            netbeansShellScript = getNetBeansShellScript(netbeansHome: netbeansHome)
+            if netbeansShellScript == nil {
+                return
+            }
+            userDefaults.set(netbeansHome, forKey: "netbeans_home")
         }
-        let netbeansShellScript = netbeansHome
-            .appendingPathComponent("bin")
-            .appendingPathComponent("netbeans")
-            .path
-        print("netbeansShellScript: \(netbeansShellScript)")
-        let fileManager = FileManager.default;
-        if (!fileManager.fileExists(atPath: netbeansShellScript)) {
-            dialogCritical(messageText: "Invalid Apache NetBeans home directory!", informativeText: "Invalid Apache NetBeans home directory!")
-            return
-        }
-        var netbeansJDKHome : String
-        do {
-            let jdks = try fileManager.contentsOfDirectory(at: JDKS_DIRECTORY, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-            print("jdks: \(jdks)")
-            netbeansJDKHome = jdks[3]
-                .appendingPathComponent("Contents")
-                .appendingPathComponent("Home")
-                .path
-            print("netbeansJDKHome: \(netbeansJDKHome)")
-        } catch {
-            print("Unexpected error: \(error).")
-            return
-        }
+        print("netbeansShellScript: \(netbeansShellScript ?? "")")
         let environment = ProcessInfo.processInfo.environment
         guard let home = environment["HOME"] else {
             return
@@ -61,10 +46,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let process = Process()
         process.environment = [
             "HOME" : home,
-            "netbeans_jdkhome" : netbeansJDKHome
+            "netbeans_jdkhome" : netbeansJDKHome!
         ]
         process.executableURL = URL(fileURLWithPath: shell)
-        process.arguments = [netbeansShellScript]
+        process.arguments = [netbeansShellScript!]
         do {
             try process.run()
             NSApplication.shared.terminate(self)
@@ -76,8 +61,68 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-    func dialogCritical(messageText: String, informativeText: String) {
+    
+    func getInstalledJDKHome(installedJDK: URL?) -> String? {
+        guard let installedJDK = installedJDK else {
+            return nil
+        }
+        let installedJDKHome = installedJDK
+            .appendingPathComponent("Contents")
+            .appendingPathComponent("Home")
+            .path
+        print("installed JDK home: \(installedJDKHome)")
+        let fileManager = FileManager.default
+        if (fileManager.fileExists(atPath: installedJDKHome)) {
+            return installedJDKHome
+        }
+        return nil
+    }
+    
+    func selectInstalledJDK() -> URL? {
+        let fileManager = FileManager.default
+        do {
+            let installedJDKs = try fileManager.contentsOfDirectory(
+                at: URL(fileURLWithPath: "/Library/Java/JavaVirtualMachines")
+                , includingPropertiesForKeys: nil
+                , options: .skipsHiddenFiles
+            )
+            print("installed JDKs: \(installedJDKs)")
+            // TODO
+            return installedJDKs[3]
+        } catch {
+            print("Unexpected error: \(error).")
+            return nil
+        }
+    }
+    
+    func getNetBeansShellScript(netbeansHome: URL?) -> String? {
+        guard let netbeansHome = netbeansHome else {
+            return nil
+        }
+        let netbeansShellScript = netbeansHome
+            .appendingPathComponent("bin")
+            .appendingPathComponent("netbeans")
+            .path
+        let fileManager = FileManager.default
+        if (fileManager.fileExists(atPath: netbeansShellScript)) {
+            return netbeansShellScript
+        }
+        return nil
+    }
+    
+    func selectNetBeansHome() -> URL? {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.title = "Please, select Apache NetBeans home directory!"
+        if (openPanel.runModal() != .OK) {
+            return nil
+        }
+        return openPanel.urls.first
+    }
+    
+    func alert(alertStyle: NSAlert.Style,messageText: String, informativeText: String) {
         let alert = NSAlert()
         alert.alertStyle = .critical
         alert.messageText = messageText
